85438a40db7cb43cea16f740dc71912eee260b6f
skotla <kotla.s@northeastern.edu>
2023-11-19 12:24:27 -0500

    UserManagement Routes implementation

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,5 +1,7 @@
-import blogRouter from './blog-routes.js'
+import blogRouter from './blog-routes.js';
+import userRouter from './UserManagement-routes.js'; 
 
-export default (app) =>{
-    app.use('/blogs', blogRouter)
-}
+export default (app) => {
+    app.use('/blogs', blogRouter); 
+    app.use('/api/users', userRouter); 
+};
