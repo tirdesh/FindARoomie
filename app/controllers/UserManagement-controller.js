14af29430e13e1904c920ed5062b31c43e24aad3
skotla <kotla.s@northeastern.edu>
2023-11-19 12:09:28 -0500

    UserManagement Services and Controllers implementation

---
+++ app/controllers/UserManagement-controller.js
@@ -0,0 +1,42 @@
+import * as userServices from '../services/usermanagementServices.js';
+import { setResponse, setConflictResponse, setUnauthorizedResponse, setErrorResponse } from './response-handler.js';
+
+export const createUser = async (request, response) => {
+    try {
+        const userData = { ...request.body };
+        const newUser = await userServices.createUser(userData);
+        setResponse(newUser, response, 201, 'User created successfully');
+    } catch (err) {
+        if (err.message === 'Email already in use') {
+            setConflictResponse('Email already in use', response);
+        } else {
+            setErrorResponse(err, response);
+        }
+    }
+};
+
+export const loginUser = async (request, response) => {
+    try {
+        const { email, password } = request.body;
+        const token = await userServices.loginUser(email, password);
+        setResponse({ token }, response, 200, 'Login successful');
+    } catch (err) {
+        if (err.message === 'Invalid login credentials') {
+            setUnauthorizedResponse('Invalid login credentials', response);
+        } else {
+            setErrorResponse(err, response);
+        }
+    }
+};
+
+export const resetPassword = async (request, response) => {
+    try {
+        const { userId, newPassword } = request.body;
+        await userServices.resetUserPassword(userId, newPassword);
+        setResponse({}, response, 200, 'Password reset successfully');
+    } catch (err) {
+        setErrorResponse(err, response);
+    }
+};
+
+
