b93333fa594ca60f1df2d02dcb9d3754184301d4
skotla <kotla.s@northeastern.edu>
2023-11-19 20:01:21 -0500

    Profile Management routes and controller implementations and change

--- app/model/index.js
+++ app/model/index.js
@@ -1,7 +1,8 @@
 import User from './UserManagement.js';
 import Blog from './blogs.js';
-
+import Profile from './ProfileManagement.js'; 
 export default {
     User,
-    Blog
+    Blog,
+    Profile 
 };
