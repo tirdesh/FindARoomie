31334cc274044c5953db504310cf8c981b33dc4b
Abhinav-1502 <145064782+Abhinav-1502@users.noreply.github.com>
2023-11-21 03:03:13 -0600

    Fully functional room api

--- app/model/index.js
+++ app/model/index.js
@@ -1,8 +1,10 @@
 import User from './UserManagement.js';
 import Blog from './blogs.js';
 import Profile from './ProfileManagement.js'; 
+import Room from './roomManagement.js'
 export default {
     User,
     Blog,
-    Profile 
+    Profile,
+    Room
 };
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,11 @@
 import blogRouter from './blog-routes.js';
 import userRouter from './UserManagement-routes.js';
 import profileRouter from './ProfileManagement-routes.js'; 
+import roomRouter from './RoomManagement-routes.js';
 
 export default (app) => {
     app.use('/blogs', blogRouter); 
     app.use('/api/users', userRouter); 
     app.use('/api', profileRouter); 
+    app.use('/rooms', roomRouter);
 };
