9093fefac7f577d14c88f023965806ec6b4738f0
tirdesh <tirdesh@gmail.com>
2023-11-21 01:45:23 -0500

    Adding RoomFilter Schema in model

---
+++ app/model/room-filters.js
@@ -0,0 +1,33 @@
+import mongoose  from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const roomFilterSchema = new Schema({
+    id: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    dateOfCreation: {
+        type: String,
+        required: true
+    },
+    filterParams: [
+        {
+            location: {type: String, required: true},
+            price_range: {type: String},
+            amenities: {type: Array},
+            lease_duration: {type: String},
+            room_size:{type: String},
+            pet_policy:{type: String},
+            available_from:{type: String}
+        }
+    ]
+});
+
+const roomFilterModel = mongoose.model('roomFilter', roomFilterSchema);
+
+export default roomFilterModel;
