7ac1ceddbd41652257f87289ee393b2026f9072f
Abhinav-1502 <145064782+Abhinav-1502@users.noreply.github.com>
2023-12-09 15:38:16 -0500

    Update Navbar.tsx

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -1,67 +1,72 @@
-import {AppBar, Button, IconButton, Stack, Toolbar, Typography} from '@mui/material';
+import { AppBar, Button, IconButton, Stack, Toolbar, Typography } from '@mui/material';
 import Diversity3Icon from '@mui/icons-material/Diversity3';
+import Brightness4Icon from '@mui/icons-material/Brightness4'; // Icon for light theme (moon)
+import Brightness7Icon from '@mui/icons-material/Brightness7'; // Icon for dark theme (sun)
 import React from 'react';
-import { Link, redirect, useNavigate } from 'react-router-dom';
-import './navbar.css'
+import { Link } from 'react-router-dom';
+import './navbar.css';
 
-function ResponsiveAppBar() {
-  let navigate = useNavigate(); 
-  const routeChange = () =>{ 
-    let path = `/Home`; 
-    navigate(path);
-  }
+interface ResponsiveAppBarProps {
+  theme: string;
+  toggleTheme: () => void;
+}
 
-  const red=() =>{
-    alert("clicked logo");
-  }
- 
+function ResponsiveAppBar({ theme, toggleTheme }: ResponsiveAppBarProps) {
   return (
-    <AppBar className='navbar-app' position='sticky'>
-        <Toolbar>
-            <IconButton onClick={routeChange} sx={{margin: 2}} size='large' edge="start" color='inherit' aria-label='logo'>
-               <Diversity3Icon/>
-            </IconButton>
+    <AppBar position="sticky" className={`navbar-app ${theme}`}>
+      <Toolbar>
+        <IconButton sx={{margin: 2}} size='large' edge="start" color='inherit' aria-label='logo'>
+          <Diversity3Icon />
+        </IconButton>
 
-            <Typography sx={{marginLeft:2, flexGrow:1}} variant='h6' component='div'>
-              <a onClick={routeChange}>Roomies</a>
-            </Typography>
+        <Typography sx={{marginLeft:2, flexGrow:1}} variant='h6' component='div'>
+          Roomies
+        </Typography>
 
-            <Stack direction={'row'} spacing={3}>
-              <Link to="/fetch">
-                <Button sx={{color: 'whitesmoke'}} color="secondary">Fetch API</Button>
-              </Link>
-              <Link to="/">
-                <Button sx={{color: 'whitesmoke'}}  color="secondary">Home</Button>
-              </Link>
-              <Link to="/about">
-                <Button sx={{color: 'whitesmoke'}}  color="secondary">AboutUs</Button>
-              </Link>
-              <Link to="/axios">
-                <Button sx={{color: 'whitesmoke'}}  color="secondary">Axios</Button>
-              </Link>
-              <Link to="/test">
-                <Button sx={{color: 'whitesmoke'}}  color="secondary">Post</Button>
-              </Link>
-              <Link to="/blogs">
+        <Stack direction={'row'} spacing={3}>
+          {/* Navigation Links and Buttons */}
+          <Link to="/fetch" style={{ textDecoration: 'none' }}>
+            <Button color="inherit">Fetch API</Button>
+          </Link>
+          <Link to="/" style={{ textDecoration: 'none' }}>
+            <Button color="inherit">Home</Button>
+          </Link>
+          <Link to="/about" style={{ textDecoration: 'none' }}>
+            <Button color="inherit">About Us</Button>
+          </Link>
+          <Link to="/axios" style={{ textDecoration: 'none' }}>
+            <Button color="inherit">Axios</Button>
+          </Link>
+          <Link to="/test" style={{ textDecoration: 'none' }}>
+            <Button color="inherit">Post</Button>
+          </Link>
+          <Link to="/blogs">
                 <Button sx={{color: 'whitesmoke'}} color="secondary">Blogs</Button>
-              </Link>
-              <Link to="/login">
-              <Button sx={{color: 'whitesmoke'}} color="secondary">Login / Sign Up</Button>
-              </Link>
-              <Link to="/upload">
-              <Button sx={{color: 'whitesmoke'}} color="secondary">Upload</Button>
-              </Link>
-              <Link to="/map">
-              <Button sx={{color: 'whitesmoke'}} color="secondary">Map</Button>
-              </Link>
-              <Link to="/chat">
-              <Button sx={{color: 'whitesmoke'}} color="secondary">Chat</Button>
-              </Link>
-            </Stack>
+          </Link>
+          <Link to="/login" style={{ textDecoration: 'none' }}>
+            <Button color="inherit">Login / Sign Up</Button>
+          </Link>
+          <Link to="/upload" style={{ textDecoration: 'none' }}>
+            <Button color="inherit">Upload</Button>
+          </Link>
+          <Link to="/map" style={{ textDecoration: 'none' }}>
+            <Button color="inherit">Map</Button>
+          </Link>
+          <Link to="/chat" style={{ textDecoration: 'none' }}>
+            <Button color="inherit">Chat</Button>
+          </Link>
+        </Stack>
 
-            
-        </Toolbar>
+        {/* Theme Toggle Button */}
+        <IconButton
+          sx={{ color: 'inherit' }}
+          onClick={toggleTheme}
+        >
+          {theme === 'light' ? <Brightness4Icon /> : <Brightness7Icon />}
+        </IconButton>
+      </Toolbar>
     </AppBar>
   );
 }
+
 export default ResponsiveAppBar;
