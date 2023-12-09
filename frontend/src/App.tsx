93df9cb3a9bc9d4e0df709e423621fb19bca1f8a
skotla <kotla.s@northeastern.edu>
2023-12-08 20:32:47 -0500

    Theme imeplementation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import { Button, AppBar, Toolbar, Typography, Container } from '@mui/material';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import ResponsiveAppBar from './components/Navbar/navbar';
 import LandingPage from './pages/LandingPage/LandingPage';
 import AboutUs from './pages/AboutUs/AboutUs'; 
 import RoomFiltersAxios from './pages/testPages/sample-apiconnector-axios';
@@ -11,34 +11,35 @@ import ImageUpload from './pages/testPages/imageUpload';
 import FilteredMap from './pages/testPages/mapViewer';
 import ChatPage from './pages/testPages/chatComponent';
 import ChatEntryPage from './pages/testPages/chatEntry';
-import Navbar from './components/Navbar/navbar';
 import Login from './pages/Login/login-page';
 
 function App() {
+  const [theme, setTheme] = useState('light'); // State to manage theme
+
+  // Function to toggle the theme
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
+  };
+
   return (
     <Router>
-      <div className="App">
-      <Navbar></Navbar>
-
+      <div className={`App ${theme}`}>
+        <ResponsiveAppBar theme={theme} toggleTheme={toggleTheme} />
         {/* Main Content Area */}
-        <Container style={{ marginTop: '20px' }}>
-          <Routes>
-            <Route path="/landing" element={<LandingPage />} />
-            {/* Add route for About Us */}
-            <Route path="/about" element={<AboutUs />} />
-            <Route path='/login' element={<Login />} />
-            <Route path="/fetch" element={<RoomFiltersFetch />} />
-            <Route path="/axios" element={<RoomFiltersAxios />} />
-            <Route path="/test" element={<CreateListingForm />} />
-            <Route path="/summary" element={<SummaryPage />} />
-            <Route path="/upload" element={<ImageUpload />} />
-            <Route path="/map" element={<FilteredMap />} />
-            <Route path="/chat" element={<ChatEntryPage />} />
-            <Route path="/chat/:roomName/:username" element={<ChatPage />} />
-            {/* Redirect to landing page as the default route */}
-            <Route path="/" element={<LandingPage />} />
-          </Routes>
-        </Container>
+        <Routes>
+          <Route path="/landing" element={<LandingPage />} />
+          <Route path="/about" element={<AboutUs />} />
+          <Route path='/login' element={<Login />} />
+          <Route path="/fetch" element={<RoomFiltersFetch />} />
+          <Route path="/axios" element={<RoomFiltersAxios />} />
+          <Route path="/test" element={<CreateListingForm />} />
+          <Route path="/summary" element={<SummaryPage />} />
+          <Route path="/upload" element={<ImageUpload />} />
+          <Route path="/map" element={<FilteredMap />} />
+          <Route path="/chat" element={<ChatEntryPage />} />
+          <Route path="/chat/:roomName/:username" element={<ChatPage />} />
+          <Route path="/" element={<LandingPage />} />
+        </Routes>
       </div>
     </Router>
   );
