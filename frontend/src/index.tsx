6dcc9917cd68087836c42b16358809da6f3ed713
tirdesh <tirdesh@gmail.com>
2023-12-13 04:40:13 -0500

    Added CustomizedAlert and ValidationProvider

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,25 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux';
-import store from './redux/store'; 
-import './index.css';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import { I18nextProvider } from 'react-i18next';
-import i18n from './i18n';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import "./index.css";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import { I18nextProvider } from "react-i18next";
+import i18n from "./i18n";
+import { AlertProvider } from "./handlers/AlertProvider";
+import { ValidationProvider } from "./handlers/ValidationProvider";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
-    <React.StrictMode>
-      <Provider store={store}>
+  <React.StrictMode>
+    <Provider store={store}>
       <I18nextProvider i18n={i18n}>
-        <App />
+        <AlertProvider>
+          <ValidationProvider>
+            <App />
+          </ValidationProvider>
+        </AlertProvider>
       </I18nextProvider>
-      </Provider>
-    </React.StrictMode>
+    </Provider>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
